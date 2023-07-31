f6277ca75c7f72f30ff92b136395083f38c81b25
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-07-31 23:25:37 +0600

    footer done

--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const MainLayout = ({ children }) => {
     return (
         <div>
             <Navbar />
             {children}
+            <Footer/>
         </div>
     );
 };
