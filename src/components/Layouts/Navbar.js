0879ebaea1cb8624c1c9dbc549b4379709509227
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-08-07 21:50:57 +0600

    deployment

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100 px-9">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">Tech Store</a>
+                <Link href="/" className="btn btn-ghost normal-case text-xl">Tech Store</Link>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
