42920cdee2b103ca17748cc4147b42e6f4bda301
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-08-07 21:53:23 +0600

    depl

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
                     <li>
-                        <Link href="/pc/buildpc">PC Builder</Link>
+                        <Link href="/pc/buildPc">PC Builder</Link>
                     </li>
                     <li>
                         <details>
